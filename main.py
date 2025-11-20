95d678bd7d6bac6346bd402bfb4b1659d517cc5a
Isaac2181 <Isaac.218@hotmail.co.uk>
2025-11-20 10:41:07 +0000

    Add files via upload

---
+++ main.py
@@ -0,0 +1,13 @@
+
+# In[1]
+import manim
+from manim import *
+# In[2]
+
+class CreateCircle(Scene):
+    def construct(self):
+        circle = Circle()  # create a circle
+        circle.set_fill(PINK, opacity=0.5)  # set the color and transparency
+        self.play(Create(circle))  # show the circle on screen
+
+# In[3]
